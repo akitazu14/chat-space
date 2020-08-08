a2fb490866affbbe046abc9b6ff41346368943ad
akitazu14 <greatarcher14@yahoo.co.jp>
2020-08-08 17:38:57 +0900

    add-increment-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def index
+    @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
-      format.html
+      # format.html
       format.json
     end
   end
